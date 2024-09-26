0616828a3d7842445c5a1a5f65e49852a5135256
Sujayjk123 <sujus7627@gmail.com>
2024-09-26 11:03:36 +0530

    My first Commit

---
+++ Variables.java
@@ -0,0 +1,38 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+      
+byte ageInYears = 28;
+byte errorCode = -5;
+byte batteryLevel = 75;
+byte numOfCores = 4;
+byte sensorReading = -12;
+byte itemsInCart = 7;
+byte dayOfWeek = 3; 
+byte signalStrength = -85;
+
+
+short currentYear = 2024;
+short maxCapacity = 2500;
+short fileSizeInMB = 512;
+short buildingHeightInMeters = 300;
+short speedLimit = 120;
+short waterDepth = -200; 
+short ticketsSold = 4500;
+short monthlyExpenses = 15000;
+
+sensorReading = -13;
+itemsInCart = 8;
+dayOfWeek = 5; 
+signalStrength = -45;
+
+speedLimit = 130;
+waterDepth = -100; 
+ticketsSold = 4000;
+monthlyExpenses = 12000;
+
+
+System.out.println(speedLimit + " " + waterDepth);
+    }
+}
