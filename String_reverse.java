19fada73d20abd1e7f0eeaa0d82f6a8507810533
Sujayjk123 <sujus7627@gmail.com>
2024-10-22 10:46:26 +0530

    Stings literals and String class

--- String_reverse.java
+++ String_reverse.java
@@ -1,33 +1,35 @@
 public class String_reverse {
     public static void main(String[] args) {
 
-        String str1 = "Hello";
-        String str2 = "World";
-        String str3 = "Java";
-        String str4 = "Programming";
-        String str5 = "JVM";
-        String str6 = "String";
-        String str7 = "Literal";
-        String str8 = "Heap";
-        String str9 = "Heap";
-        String str10 = "Pool";
         
+    String greeting = "Hello";
+    String planet = "World";
+    String language = "Java";
+    String concept = "Programming";
+    String runtimeEnv = "JVM";
+    String dataType = "String";
+    String description = "Literal";
+    String memoryType = "Heap";
+    String duplicateMemoryType = "Heap"; 
+    String storageArea = "Pool";
+
+
+    String newGreeting = new String("Hello");
+    String newPlanet = new String("World");
+    String newLanguage = new String("Java");
+    String newConcept = new String("Programming");
+    String newRuntimeEnv = new String("JVM");
+    String newDataType = new String("String");
+    String newDescription = new String("Literal");
+    String newEntity = new String("Object");
+    String newMemoryType = new String("Heap");
+    String newStorageArea = new String("Pool");
+
         
-        String newStr1 = new String("Hello");
-        String newStr2 = new String("World");
-        String newStr3 = new String("Java");
-        String newStr4 = new String("Programming");
-        String newStr5 = new String("JVM");
-        String newStr6 = new String("String");
-        String newStr7 = new String("Literal");
-        String newStr8 = new String("Object");
-        String newStr9 = new String("Heap");
-        String newStr10 = new String("Pool");
-        
-        System.out.println(checkEqual(str8,str9));
-        System.out.println(checkEqual(newStr1,newStr10));
-        System.out.println(reverseString(str7));
-        System.out.println(reverseString(newStr4));
+    System.out.println(checkEqual(greeting,planet));
+    
+    System.out.println(reverseString(newConcept));
+    System.out.println(reverseString(newDescription));
     }
 
     private static String reverseString(String str4) {
